f776f3b3ec9b99828c60b8a1336b71b53c8301cb
Alex Ford <alex@crushpath.com>
2014-06-09 18:18:52 -0700

    Listen to iframe posting message that it should be closed

--- chat-beacon.js
+++ chat-beacon.js
@@ -27,25 +27,39 @@ $(function() {
 
   ////////
   // click handler on chat bubble
-  $(".js-show-chat").click(function() {
-    if (chatFrameOpened) {
-      $(".js-chat-iframe-container").fadeOut(125, function() {
-        chatFrameOpened = false;
-      });
-    } else {
-      $(".js-chat-iframe-container").fadeIn(125, function() {
-        chatFrameOpened = true;
-        unreadCount = 0;      // set "new" to 0 when tool opened
-        updateUnreadBadge();
-      });
+  $(".js-show-chat").click(chatFrameOpened ? closeChatFrame : openChatFrame);
 
-      if (!chatFrameLoaded) {
-        $(".js-chat-iframe-container iframe").attr('src',chatBeaconBootstrap.streamAppURL)[0].onload = function() {
-          chatFrameLoaded = true;
-        };
-      }
+  ////////
+  // listen to window message to close chat frame
+  window.addEventListener("message", function(e) {
+    if (e.data === 'closeChatDropdown') {
+      closeChatFrame();
     }
-  });
+  }, false);
+
+  ////////
+  // Close dropdown
+  function closeChatFrame() {
+    $(".js-chat-iframe-container").fadeOut(125, function() {
+      chatFrameOpened = false;
+    });
+  }
+
+  ////////
+  // Open dropdown
+  function openChatFrame() {
+    $(".js-chat-iframe-container").fadeIn(125, function() {
+      chatFrameOpened = true;
+      unreadCount = 0;      // set "new" to 0 when tool opened
+      updateUnreadBadge();
+    });
+
+    if (!chatFrameLoaded) {
+      $(".js-chat-iframe-container iframe").attr('src',chatBeaconBootstrap.streamAppURL)[0].onload = function() {
+        chatFrameLoaded = true;
+      };
+    }
+  }
 
   ////////
   // Update counter badge in DOM
